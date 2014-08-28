b0f10cd48aea9536d9010ae13e33801c5606ae46
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-28 12:25:59 -0600

    Use loader.js

--- Brocfile.js
+++ Brocfile.js
@@ -2,9 +2,24 @@ var compileModules = require('broccoli-compile-modules'),
     moveFile       = require('broccoli-file-mover'),
     uglify         = require('broccoli-uglify-js'),
     mergeTrees     = require('broccoli-merge-trees'),
-    es6            = require('broccoli-es6-concatenator');
+    es6            = require('broccoli-es6-concatenator'),
+    pickFiles      = require('broccoli-static-compiler');
 
-var jsTree = es6('src', {
+// var bower = pickFiles('bower_components', {
+//   srcDir: '/',
+//   inputFiles: [ '**/*' ],
+//   destDir: '/bower_components'
+// });
+
+var loaderJS = pickFiles('bower_components/loader.js', {
+  srcDir: '/',
+  files: [ 'loader.js' ],
+  destDir: '/'
+});
+
+var jsTree = mergeTrees(['src', loaderJS]);
+jsTree = es6(jsTree, {
+  loaderFile: 'loader.js',
   inputFiles: ['eigensheep.js'],
   wrapInEval: false,
   outputFile: '/eigensheep.prod.js'
