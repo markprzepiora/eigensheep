ebffe8bb310993a3d54df93633ece4e8e14d354f
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-28 12:28:40 -0600

    Back to using broccoli-compile-modules for now :)

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,8 @@ var compileModules = require('broccoli-compile-modules'),
     uglify         = require('broccoli-uglify-js'),
     mergeTrees     = require('broccoli-merge-trees'),
     es6            = require('broccoli-es6-concatenator'),
-    pickFiles      = require('broccoli-static-compiler');
+    pickFiles      = require('broccoli-static-compiler'),
+    wrap           = require('broccoli-wrap');
 
 // var bower = pickFiles('bower_components', {
 //   srcDir: '/',
@@ -17,19 +18,19 @@ var loaderJS = pickFiles('bower_components/loader.js', {
   destDir: '/'
 });
 
-var jsTree = mergeTrees(['src', loaderJS]);
-jsTree = es6(jsTree, {
-  loaderFile: 'loader.js',
-  inputFiles: ['eigensheep.js'],
-  wrapInEval: false,
-  outputFile: '/eigensheep.prod.js'
-});
-
-// jsTree = compileModules('src', {
+// var jsTree = mergeTrees(['src', loaderJS]);
+// jsTree = es6(jsTree, {
+//   loaderFile: 'loader.js',
 //   inputFiles: ['eigensheep.js'],
-//   output: '/eigensheep.prod.js',
-//   formatter: 'bundle'
+//   wrapInEval: false,
+//   outputFile: '/eigensheep.prod.js'
 // });
+
+var jsTree = compileModules('src', {
+  inputFiles: ['eigensheep.js'],
+  output:     '/eigensheep.prod.js',
+  formatter:  'bundle'
+});
 
 minJsTree = moveFile(jsTree, {
   srcFile: '/eigensheep.prod.js',
