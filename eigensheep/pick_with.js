23dc2b2cb2034820952f6336cf1880bee5ec147a
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-30 10:58:46 -0600

    Fix pickWith documentation

--- eigensheep/pick_with.js
+++ eigensheep/pick_with.js
@@ -9,14 +9,16 @@ import curry2 from "./internal/curry2";
  * @memberOf R
  * @category Object
  * @param {Function} pred A predicate to determine whether or not a key
- *        should be included on the output object.
+ *        should be included on the output object. This predicate receives
+ *        three arguments: `val`, `key`, and `object`, which are the value and
+ *        and key in question, and a reference to the original object.
  * @param {Object} obj The object to copy from
  * @return {Object} A new object with only properties that satisfy `pred`
  *         on it.
  * @see pick
  * @example
  *
- *      function isUpperCase(x) { return x.toUpperCase() === x; }
+ *      function isUpperCase(val, key, object) { return key.toUpperCase() === key; }
  *      pickWith(isUpperCase, {a: 1, b: 2, A: 3, B: 4}) // => {A: 3, B: 4}
  */
 var pickWith = curry2(_pickWith);
