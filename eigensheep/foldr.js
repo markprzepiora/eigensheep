a06681d99c1bc3e832333a2882bc25822b82db46
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-30 00:26:12 -0600

    Add foldr

---
+++ eigensheep/foldr.js
@@ -0,0 +1,43 @@
+import curry3 from "./internal/curry3";
+import checkForMethod from "./internal/check_for_method";
+
+/**
+ * Returns a single item by iterating through the list, successively calling the iterator
+ * function and passing it an accumulator value and the current value from the array, and
+ * then passing the result to the next call.
+ *
+ * Similar to `foldl`, except moves through the input list from the right to the left.
+ *
+ * The iterator function receives two values: *(acc, value)*
+ *
+ * Note: `ramda.foldr` does not skip deleted or unassigned indices (sparse arrays), unlike
+ * the native `Array.prototype.reduce` method. For more details on this behavior, see:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce#Description
+ *
+ * @static
+ * @memberOf R
+ * @category List
+ * @param {Function} fn The iterator function. Receives two values, the accumulator and the
+ *        current element from the array.
+ * @param {*} acc The accumulator value.
+ * @param {Array} list The list to iterate over.
+ * @return {*} The final, accumulated value.
+ * @alias reduceRight
+ * @example
+ *
+ *      var pairs = [ ['a', 1], ['b', 2], ['c', 3] ];
+ *      var flattenPairs = function(acc, pair) {
+ *        return acc.concat(pair);
+ *      };
+ *
+ *      foldr(numbers, flattenPairs, []); //=> [ 'c', 3, 'b', 2, 'a', 1 ]
+ */
+var foldr = curry3(checkForMethod('foldr', function(fn, acc, list) {
+    var idx = list.length;
+    while (idx--) {
+        acc = fn(acc, list[idx]);
+    }
+    return acc;
+}));
+
+export default foldr;
--- test/eigensheep/foldr_test.js
+++ test/eigensheep/foldr_test.js
@@ -1,33 +1,33 @@
 (function() {
-  module("R.foldl");
+  module("R.foldr");
 
-  var foldl = require('eigensheep/foldl')['default'];
+  var foldr = require('eigensheep/foldr')['default'];
   var add = function(a, b) { return a + b };
 
   test("summing an array", function() {
     var numbers = [1, 2, 3];
 
-    equal(foldl(add, 0, numbers), 6);
+    equal(foldr(add, 0, numbers), 6);
   });
 
   test("a different accumulator value", function() {
     var numbers = [1, 2, 3];
 
-    equal(foldl(add, 10, numbers), 16);
+    equal(foldr(add, 10, numbers), 16);
   });
 
   test("creating a sum function", function() {
     var numbers = [1, 2, 3];
-    var sum = foldl(add, 0);
+    var sum = foldr(add, 0);
 
     equal(sum(numbers), 6);
   });
 
-  test("folding from left to right", function() {
+  test("folding from right to left", function() {
     var letters = ['h', 'e', 'y', 'a'];
     var concat = function(a, b) { return a + b };
-    var stringifier = foldl(concat, '');
+    var reverser = foldr(concat, '');
 
-    equal(stringifier(letters), 'heya');
+    equal(reverser(letters), 'ayeh');
   });
 })();
