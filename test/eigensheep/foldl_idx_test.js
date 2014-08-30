0ecd3531e50d9ef3c33c4cab66f64527cd23253b
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-30 00:18:41 -0600

    Test currying in foldl.idx

--- test/eigensheep/foldl_idx_test.js
+++ test/eigensheep/foldl_idx_test.js
@@ -15,4 +15,14 @@
       { 'a': 0, 'b': 1, 'c': 2 }
     );
   });
+
+  test("creating a curried reducer", function() {
+    var numbers = [1, 2, 3];
+    var iterator = function(number, elem, idx, list) {
+      return number + elem / Math.pow(10, idx);
+    };
+    var fractionizer = foldl.idx(iterator, 0);
+
+    equal(fractionizer(numbers), 1.23);
+  });
 })();
