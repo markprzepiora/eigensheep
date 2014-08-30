c6be989957426ec80e5e3b53f9639d9348b9f8ac
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-29 21:36:08 -0600

    Add reduce and tests

---
+++ eigensheep/internal/curry3.js
@@ -0,0 +1,32 @@
+import curry2 from "./curry2";
+import NO_ARGS_EXCEPTION from "./no_args_exception";
+
+/**
+ * Optimized internal three-arity curry function.
+ *
+ * @private
+ * @category Function
+ * @param {Function} fn The function to curry.
+ * @return {Function} curried function
+ * @example
+ *
+ *      var addThree = function(a, b, c) {
+ *        return a + b + c;
+ *      };
+ *
+ *      var curriedAddThree = curry3(addThree);
+ */
+export default function curry3(fn) {
+    return function(a, b, c) {
+        switch (arguments.length) {
+            case 0: throw NO_ARGS_EXCEPTION;
+            case 1: return curry2(function(b, c) {
+                return fn(a, b, c);
+            });
+            case 2: return function(c) {
+                return fn(a, b, c);
+            };
+            default: return fn(a, b, c);
+        }
+    };
+}
