79b7a6a1b20e62abed4a518ca8ec083a58808202
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-30 09:59:06 -0600

    Add filter.idx

--- eigensheep/filter.js
+++ eigensheep/filter.js
@@ -1,4 +1,5 @@
 import _filter from "./internal/filter";
+import _filterIdx from "./internal/filter_idx";
 import curry2 from "./internal/curry2";
 import checkForMethod from "./internal/check_for_method";
 
@@ -24,5 +25,24 @@ import checkForMethod from "./internal/check_for_method";
  *      var evens = ramda.filter(isEven, [1, 2, 3, 4]); // => [2, 4]
  */
 var filter = curry2(checkForMethod('filter', _filter));
+
+/**
+ * Like `filter`, but passes additional parameters to the predicate function. The predicate
+ * function is passed three arguments: *(value, index, list)*.
+ *
+ * @static
+ * @memberOf R
+ * @category List
+ * @param {Function} fn The function called per iteration.
+ * @param {Array} list The collection to iterate over.
+ * @return {Array} The new filtered array.
+ * @example
+ *
+ *      var lastTwo = function(val, idx, list) {
+ *        return list.length - idx <= 2;
+ *      };
+ *      ramda.filter.idx(lastTwo, [8, 6, 7, 5, 3, 0, 9]); //=> [0, 9]
+ */
+filter.idx = curry2(checkForMethod('filter', _filterIdx));
 
 export default filter;
---
+++ eigensheep/internal/filter_idx.js
@@ -0,0 +1,9 @@
+export default function _filterIdx(fn, list) {
+    var idx = -1, len = list.length, result = [];
+    while (++idx < len) {
+        if (fn(list[idx], idx, list)) {
+            result.push(list[idx]);
+        }
+    }
+    return result;
+}
