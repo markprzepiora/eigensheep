04c115a37d6b50a3fdf32437749f4db4ac454fd2
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 15:51:26 -0600

    Add construct

---
+++ eigensheep/construct.js
@@ -0,0 +1,27 @@
+import constructN from "./construct_n";
+
+/**
+ * Wraps a constructor function inside a curried function that can be called with the same
+ * arguments and returns the same type.
+ *
+ * NOTE: Does not work with some built-in objects such as Date.
+ *
+ * @static
+ * @memberOf R
+ * @category Function
+ * @param {Function} Fn The constructor function to wrap.
+ * @return {Function} A wrapped, curried constructor function.
+ * @example
+ *
+ *      // Constructor function
+ *      var Widget = function(config) {
+ *        // ...
+ *      };
+ *      Widget.prototype = {
+ *        // ...
+ *      };
+ *      map(construct(Widget), allConfigs); //=> a list of Widgets
+ */
+export default function construct(Fn) {
+    return constructN(Fn.length, Fn);
+};
