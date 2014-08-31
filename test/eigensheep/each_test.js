75919821f4f6dd3c82a6bf5883e5fc68ebc74baa
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-30 20:19:13 -0600

    Wrap each test in an IIFE

--- test/eigensheep/each_test.js
+++ test/eigensheep/each_test.js
@@ -1,14 +1,16 @@
-module("R.each");
+(function() {
+  module("R.each");
 
-var each = require('eigensheep/each')['default'];
+  var each = require('eigensheep/each')['default'];
 
-test("it executes the callback once for each element in the array", function() {
-  expect(3);
+  test("it executes the callback once for each element in the array", function() {
+    expect(3);
 
-  var count = 0;
-  var callback = function(x) {
-    equal(x, ++count, "the callback gets called with " + count);
-  };
+    var count = 0;
+    var callback = function(x) {
+      equal(x, ++count, "the callback gets called with " + count);
+    };
 
-  each(callback, [1, 2, 3]);
-});
+    each(callback, [1, 2, 3]);
+  });
+})();
