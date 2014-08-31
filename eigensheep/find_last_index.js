f9a14ec30e799ad570d7659a2aed61676f218f46
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 13:07:29 -0600

    Add findLastIndex with empty array bug and 1-element array bug

---
+++ eigensheep/find_last_index.js
@@ -0,0 +1,30 @@
+import curry2 from "./internal/curry2";
+
+/**
+ * Returns the index of the last element of the list which matches the predicate, or
+ * `-1` if no element matches.
+ *
+ * @static
+ * @memberOf R
+ * @category List
+ * @param {Function} fn The predicate function used to determine if the element is the
+ * desired one.
+ * @param {Array} list The array to consider.
+ * @return {number} The index of the element found, or `-1`.
+ * @example
+ *
+ *      var xs = [{a: 1, b: 0}, {a:1, b: 1}];
+ *      findLastIndex(propEq('a', 1))(xs); //= 1
+ *      findLastIndex(propEq('a', 4))(xs); //= -1
+ */
+var findLastIndex = curry2(function _findLastIndex(fn, list) {
+    var idx = list.length;
+    while (--idx) {
+        if (fn(list[idx])) {
+            return idx;
+        }
+    }
+    return -1;
+});
+
+export default findLastIndex;
---
+++ test/eigensheep/find_last_index_test.js
@@ -0,0 +1,47 @@
+(function() {
+  module("R.findLastIndex");
+
+  var findLastIndex = require('eigensheep/find_last_index')['default'];
+  var isEven = function(n) { return n % 2 === 0 };
+
+  test("returns the index of the last match of a predicate in an array", function() {
+    var findLastEvenIndex = findLastIndex(isEven);
+
+    equal(
+      findLastEvenIndex([1, 2, 3, 4]),
+      3
+    );
+
+    equal(
+      findLastEvenIndex([4, 2, 0, 10, 8, 3, 5]),
+      4
+    );
+  });
+
+  test("returns -1 if there is no match", function() {
+    var findLastEvenIndex = findLastIndex(isEven);
+
+    equal(
+      findLastEvenIndex([3, 1]),
+      -1
+    );
+  });
+
+  // test("works when the first element matches", function() {
+  //   var findLastEvenIndex = findLastIndex(isEven);
+
+  //   equal(
+  //     findLastEvenIndex([2, 3, 5]),
+  //     0
+  //   );
+  // });
+
+//   test("doesn't go into an infinite loop on an empty array", function() {
+//     var findLastEvenIndex = findLastIndex(isEven);
+
+//     equal(
+//       findLastEvenIndex([]),
+//       -1
+//     );
+//   });
+})();
