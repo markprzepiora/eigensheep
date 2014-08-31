782117ec41643a8c7e4acbfac96b983f47c26730
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 11:11:33 -0600

    Add partition

---
+++ eigensheep/partition.js
@@ -0,0 +1,26 @@
+import curry2 from "./internal/curry2";
+import foldl from "./foldl";
+
+/**
+ * Takes a predicate and a list and returns the pair of lists of
+ * elements which do and do not satisfy the predicate, respectively.
+ *
+ * @static
+ * @memberOf R
+ * @param {Function} pred Function :: a -> Boolean
+ * @param {Array} list The array to partition
+ * @return {Array} A nested array, containing first an array of elements that satisfied the predicate,
+ *                 and second an array of elements that did not satisfy.
+ * @example
+ *
+ *      partition(contains('s'), ['sss', 'ttt', 'foo', 'bars'])
+ *     // => [ [ 'sss', 'bars' ],  [ 'ttt', 'foo' ] ]
+ */
+var partition = curry2(function _partition(pred, list) {
+    return foldl(function (acc, elt) {
+        acc[pred(elt) ? 0 : 1].push(elt);
+        return acc;
+    }, [[], []], list);
+});
+
+export default partition;
