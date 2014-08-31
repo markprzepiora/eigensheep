639eb5fa151d760327da0abc36218cd157aec826
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 17:59:32 -0600

    Add modulo

---
+++ eigensheep/modulo.js
@@ -0,0 +1,25 @@
+import curry2 from "./internal/curry2";
+
+/**
+ * Divides the second parameter by the first and returns the remainder.
+ * The flipped version (`moduloBy`) may be more useful curried.
+ * Note that this functions preserves the JavaScript-style behavior for
+ * modulo. For mathematical modulo see `mathMod`
+ *
+ * @static
+ * @memberOf R
+ * @category math
+ * @param {number} a The value to the divide.
+ * @param {number} b The pseudo-modulus
+ * @return {number} The result of `b % a`.
+ * @see moduloBy, mathMod
+ * @example
+ *
+ *      modulo(17, 3) // => 2
+ *      // JS behavior:
+ *      modulo(-17, 3) // => -2
+ *      modulo(17, -3) // => 2
+ */
+var modulo = curry2(function _modulo(a, b) { return a % b; });
+
+export default modulo;
