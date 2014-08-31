560ced63d06ac811576717ec2f1d3e3ccfb67812
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 15:49:25 -0600

    Add constructN

---
+++ eigensheep/construct_n.js
@@ -0,0 +1,38 @@
+import curry from "./curry";
+import nAry from "./n_ary";
+
+/**
+ * Wraps a constructor function inside a curried function that can be called with the same
+ * arguments and returns the same type. The arity of the function returned is specified
+ * to allow using variadic constructor functions.
+ *
+ * NOTE: Does not work with some built-in objects such as Date.
+ *
+ * @static
+ * @memberOf R
+ * @category Function
+ * @param {number} n The arity of the constructor function.
+ * @param {Function} Fn The constructor function to wrap.
+ * @return {Function} A wrapped, curried constructor function.
+ * @example
+ *
+ *      // Variadic constructor function
+ *      var Widget = function() {
+ *        this.children = Array.prototype.slice.call(arguments);
+ *        // ...
+ *      };
+ *      Widget.prototype = {
+ *        // ...
+ *      };
+ *      map(constructN(1, Widget), allConfigs); //=> a list of Widgets
+ */
+export default function constructN(n, Fn) {
+    var f = function () {
+        var Temp = function() {}, inst, ret;
+        Temp.prototype = Fn.prototype;
+        inst = new Temp();
+        ret = Fn.apply(inst, arguments);
+        return Object(ret) === ret ? ret : inst;
+    };
+    return n > 1 ? curry(nAry(n, f)) : f;
+};
