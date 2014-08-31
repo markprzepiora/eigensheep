34c8a7257c65db36dc643412a6e820e04fb46fe1
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 15:05:52 -0600

    Add uniqWith

---
+++ eigensheep/uniq_with.js
@@ -0,0 +1,34 @@
+import curry2 from "./internal/curry2";
+import _containsWith from "./internal/contains_with";
+
+/**
+ * Returns a new list containing only one copy of each element in the original list, based
+ * upon the value returned by applying the supplied predicate to two list elements. Prefers
+ * the first item if two items compare equal based on the predicate.
+ *
+ * @static
+ * @memberOf R
+ * @category List
+ * @param {Array} list The array to consider.
+ * @return {Array} The list of unique items.
+ * @example
+ *
+ * var strEq = function(a, b) { return ('' + a) === ('' + b) };
+ * uniqWith(strEq)([1, '1', 2, 1]); //= [1, 2]
+ * uniqWith(strEq)([{}, {}]);       //= [{}]
+ * uniqWith(strEq)([1, '1', 1]);    //= [1]
+ * uniqWith(strEq)(['1', 1, 1]);    //= ['1']
+ */
+var uniqWith = curry2(function _uniqWith(pred, list) {
+    var idx = -1, len = list.length;
+    var result = [], item;
+    while (++idx < len) {
+        item = list[idx];
+        if (!_containsWith(pred, item, result)) {
+            result.push(item);
+        }
+    }
+    return result;
+});
+
+export default uniqWith;
