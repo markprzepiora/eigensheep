b274a9007804fa79e639d894c18287f05461be47
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-30 18:23:19 -0600

    Add func

---
+++ eigensheep/func.js
@@ -0,0 +1,28 @@
+import NO_ARGS_EXCEPTION from "./internal/no_args_exception";
+import _slice from "./internal/slice";
+
+/**
+ * Calls the specified function on the supplied object. Any additional arguments
+ * after `fn` and `obj` are passed in to `fn`. If no additional arguments are passed to `func`,
+ * `fn` is invoked with no arguments.
+ *
+ * @static
+ * @memberOf R
+ * @category Object
+ * @param {String} fn The name of the property mapped to the function to invoke
+ * @param {Object} obj The object
+ * @return {*} The value of invoking `obj.fn`
+ * @example
+ *
+ *      func('add', R, 1, 2) // => 3
+ *
+ *      var obj = { f: function() { return 'f called'; } };
+ *      func('f', obj); // => 'f called'
+ */
+export default function func(fn, obj) { // TODO: change param name: reserve `fn` for functions, not names?
+    switch (arguments.length) {
+        case 0: throw NO_ARGS_EXCEPTION;
+        case 1: return function(obj) { return obj[fn].apply(obj, _slice(arguments, 1)); };
+        default: return obj[fn].apply(obj, _slice(arguments, 2));
+    }
+};
