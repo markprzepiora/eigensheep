fbf3b7156e562c5ec3ab47306e6c9ff64a2e3211
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-30 20:51:31 -0600

    Add tail

---
+++ eigensheep/tail.js
@@ -0,0 +1,24 @@
+import checkForMethod from "./internal/check_for_method";
+import _slice from "./internal/slice";
+
+/**
+ * Returns all but the first element of a list. If the list provided has the `tail` method,
+ * it will instead return `list.tail()`.
+ *
+ * @static
+ * @memberOf R
+ * @category Array
+ * @param {Array} [arr=[]] The array to consider.
+ * @return {Array} A new array containing all but the first element of the input list, or an
+ *         empty list if the input list is a falsy value (e.g. `undefined`).
+ * @alias cdr
+ * @example
+ *
+ *      ramda.tail(['fi', 'fo', 'fum']); //=> ['fo', 'fum']
+ */
+var tail = checkForMethod('tail', function(arr) {
+    arr = arr || [];
+    return (arr.length > 1) ? _slice(arr, 1) : [];
+});
+
+export default tail;
