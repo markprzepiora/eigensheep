24a466231cdba980cd23e2705d52d3d482679240
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 09:52:22 -0600

    Add extend

---
+++ eigensheep/extend.js
@@ -0,0 +1,27 @@
+import keys from "./keys";
+
+// TODO: Any objections to currying this function?
+
+/**
+ * Assigns own enumerable properties of the other object to the destination
+ * object prefering items in other.
+ *
+ * @private
+ * @memberOf R
+ * @category Object
+ * @param {Object} object The destination object.
+ * @param {Object} other The other object to merge with destination.
+ * @returns {Object} Returns the destination object.
+ * @example
+ *
+ *      extend({ 'name': 'fred', 'age': 10 }, { 'age': 40 });
+ *      // => { 'name': 'fred', 'age': 40 }
+ */
+export default function extend(destination, other) {
+    var props = keys(other),
+        i = -1, length = props.length;
+    while (++i < length) {
+        destination[props[i]] = other[props[i]];
+    }
+    return destination;
+}
