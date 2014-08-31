4c9bd1bd0757d16ac15c46bfdc32a8d2dd5d2e1b
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-30 20:30:15 -0600

    Add isEmpty

---
+++ eigensheep/is_empty.js
@@ -0,0 +1,19 @@
+/**
+ * Reports whether an array is empty.
+ *
+ * @static
+ * @memberOf R
+ * @category Array
+ * @param {Array} arr The array to consider.
+ * @return {boolean} `true` if the `arr` argument has a length of 0 or
+ *         if `arr` is a falsy value (e.g. undefined).
+ * @example
+ *
+ *      ramda.isEmpty([1, 2, 3]); //=> false
+ *      ramda.isEmpty([]); //=> true
+ *      ramda.isEmpty(); //=> true
+ *      ramda.isEmpty(null); //=> true
+ */
+export default function isEmpty(arr) {
+    return !arr || !arr.length;
+}
