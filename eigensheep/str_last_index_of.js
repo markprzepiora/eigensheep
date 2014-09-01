96fca8f4cf83369a414e17dca464021a32e32fad
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 22:33:55 -0600

    Add strLastIndexOf

---
+++ eigensheep/str_last_index_of.js
@@ -0,0 +1,20 @@
+import invoker from "./invoker";
+
+/**
+ *
+ * Finds the last index of a substring in a string, returning -1 if it's not present
+ *
+ * @static
+ * @memberOf R
+ * @category string
+ * @param {String} c A string to find.
+ * @param {String} str The string to search in
+ * @return {Number} The last index of `c` or -1 if not found
+ * @see invoker
+ * @example
+ *
+ *      strLastIndexOf('a', 'banana split') //=> 5
+ */
+var strLastIndexOf = invoker('lastIndexOf', String.prototype);
+
+export default strLastIndexOf;
