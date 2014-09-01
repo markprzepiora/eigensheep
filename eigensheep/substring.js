cde987aacc53d907116f4ee856146b75445ad0b6
Mark Przepiora <mark.przepiora@gmail.com>
2014-08-31 19:26:59 -0600

    Add substring

---
+++ eigensheep/substring.js
@@ -0,0 +1,20 @@
+import invoker from "./invoker";
+
+/**
+ * returns a subset of a string between one index and another.
+ *
+ * @static
+ * @memberOf R
+ * @category string
+ * @param {Number} indexA An integer between 0 and the length of the string.
+ * @param {Number} indexB An integer between 0 and the length of the string.
+ * @param {String} The string to extract from
+ * @return {String} the extracted substring
+ * @see invoker
+ * @example
+ *
+ *      substring(2, 5, 'abcdefghijklm'); //=> 'cde'
+ */
+var substring = invoker('substring', String.prototype);
+
+export default substring;
